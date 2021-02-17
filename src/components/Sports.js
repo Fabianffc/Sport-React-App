ea6329203fe22a8274d5bc7a05a38d15259747c2
Fabian <fabianffc@gmail.com>
2021-02-16 20:39:27 -0600

    adding sports into the sports.js file

---
+++ src/components/Sports.js
@@ -0,0 +1,23 @@
+import { Route, NavLink, useRouteMatch } from "react-router-dom";
+import sportsData from '../data/sportsData'
+import Sport from '../components/Sport'
+
+
+export default function Sports() {
+  let { url } = useRouteMatch();
+  const links = sportsData.map(sport => (
+    <li key={sport.id}>
+      <NavLink to={`${url}/${sport.id}`}>{sport.title}</NavLink>
+    </li>
+  ))
+  return (
+    <div>
+      <h2>Sports</h2>
+      <ul>{links}</ul>
+
+      <Route path={`${url}/:id`}>
+        <Sport sportsData={sportsData} />
+      </Route>
+    </div>
+  );
+}
---
+++ src/components/Weather.js
@@ -0,0 +1,4 @@
+export default function Weather() {
+    return <h2>Weather</h2>
+    
+}
